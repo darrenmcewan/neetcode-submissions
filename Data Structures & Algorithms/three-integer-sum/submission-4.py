c61c1e31152b6f48b1bf6612ac528f1f28cc7dc7
Darren McEwan <darren@mcewan.me>
2026-05-26 08:29:33 -0700

    Add: three-integer-sum - submission-4

---
+++ Data Structures & Algorithms/three-integer-sum/submission-4.py
@@ -0,0 +1,26 @@
+class Solution:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        # find the complement of (x2,x3) for a fixed x1
+        res = []
+
+        nums.sort()
+
+        for i, num in enumerate(nums):
+            if i > 0 and nums[i] == nums[i-1]:
+                continue
+            l, r = i+1, len(nums)-1
+            while l < r:
+
+                if -(nums[l] + nums[r]) == num:
+                    res.append([nums[i], nums[l], nums[r]])
+                    l+=1
+                    r-=1
+                    while l<r and nums[l]==nums[l-1]:
+                        l+=1
+
+                elif -(nums[l] + nums[r]) < num:
+                    r-=1
+                else:
+                    l+=1
+        return list(res)
+            
