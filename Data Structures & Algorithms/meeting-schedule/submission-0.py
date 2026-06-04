688a098e8a1168aca6ee80f693d81acca1e17eff
Darren McEwan <darren@mcewan.me>
2026-06-04 07:14:35 -0700

    Add: meeting-schedule - submission-0

---
+++ Data Structures & Algorithms/meeting-schedule/submission-0.py
@@ -0,0 +1,20 @@
+"""
+Definition of Interval:
+class Interval(object):
+    def __init__(self, start, end):
+        self.start = start
+        self.end = end
+"""
+
+class Solution:
+    def canAttendMeetings(self, intervals: List[Interval]) -> bool:
+        if len(intervals) < 2:
+            return True
+        intervals.sort(key=lambda x: x.start)
+        
+        for i, meeting in enumerate(intervals):
+            if i == 0:
+                continue
+            if intervals[i-1].end > intervals[i].start:
+                return False
+        return True
